532fefbe00f1ac50e71361ca3249f6ef8eb36b38
João Luiz Lorencetti <jll.linux@gmail.com>
2018-06-06 23:17:59 -0300

    request data from server

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,81 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import 'models.dart';
 
 void main() => runApp(new MyApp());
 
-// Placeholder data
-final listPetTest = List<Pet>.generate(20, (int index) {
-  return Pet(
-    name: 'Testing Pet ${index + 1}',
-    description:
-        "Eat owner's food need to chase tail sit and stare, sleep everywhere, but not in my bed or slap owner's face at 5am until human fills food dish. Stare at the wall, play with food and get confused by dust while happily ignoring when being called.",
-    imageUrl: 'https://placekitten.com/350/350',
-    status: 'Adopted',
-  );
-});
-
 Widget _homePage() {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Cadê meu Bicho'),
+    ),
+    body: PetList(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+      ),
+      home: _homePage(),
+    );
+  }
+}
+
+class PetList extends StatefulWidget {
+  @override
+  _PetListState createState() => _PetListState();
+}
+
+class _PetListState extends State<PetList> {
+  // Placeholder data
+  var listPetTest = List<Pet>.generate(20, (int index) {
+    return Pet(
+      name: 'Testing Pet ${index + 1}',
+      description:
+          "Eat owner's food, chase tail sit and stare, sleep everywhere.",
+      imageUrl: 'https://placekitten.com/350/350',
+      status: 'Adopted',
+      city: 'Araras',
+    );
+  });
+
   Widget _itemBuilder(BuildContext context, int index) {
     final pet = listPetTest[index];
 
     Widget _petImage = Image.network(
       pet.imageUrl,
+      width: 100.0,
       height: 100.0,
+      fit: BoxFit.cover,
     );
 
-    Widget _cardBody = Padding(
-      padding: const EdgeInsets.only(left: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            pet.name,
-            style: Theme.of(context).textTheme.title,
-          ),
-          Text(
-            pet.status,
-          ),
-          Text(
-            'Araras - SP',
-          ),
-        ],
+    Widget _cardBody = Flexible(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              pet.name,
+              style: Theme.of(context).textTheme.title,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              pet.status,
+            ),
+            Text(
+              pet.city,
+            ),
+          ],
+        ),
       ),
     );
 
@@ -63,31 +100,28 @@ Widget _homePage() {
     );
   }
 
-  Widget petList = ListView.builder(
-    itemCount: listPetTest.length,
-    itemBuilder: _itemBuilder,
-  );
-
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Cadê meu Bicho'),
-    ),
-    body: RefreshIndicator(
-      onRefresh: () async {},
-      child: petList,
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.green[900],
+    return RefreshIndicator(
+      onRefresh: () async {
+        final response = await http.get('https://cademeubicho.com/api/pets/');
+        final responseJson =
+            json.decode(Utf8Decoder().convert(response.bodyBytes));
+        final pets = responseJson['results'];
+
+        var newList = <Pet>[];
+        for (var petData in pets) {
+          newList.add(Pet.fromJson(petData));
+        }
+
+        setState(() {
+          listPetTest = newList;
+        });
+      },
+      child: ListView.builder(
+        itemCount: listPetTest.length,
+        itemBuilder: _itemBuilder,
       ),
-      home: _homePage(),
     );
   }
 }
--- lib/models.dart
+++ lib/models.dart
@@ -3,6 +3,18 @@ class Pet {
   final String description;
   final String imageUrl;
   final String status;
+  final String city;
 
-  const Pet({this.name, this.description, this.imageUrl, this.status});
+  const Pet(
+      {this.name, this.description, this.imageUrl, this.status, this.city});
+
+  factory Pet.fromJson(Map<String, dynamic> json) {
+    return Pet(
+      name: json['name'],
+      description: json['description'],
+      imageUrl: json['profile_picture'],
+      status: json['status'],
+      city: json['city']['name'],
+    );
+  }
 }
